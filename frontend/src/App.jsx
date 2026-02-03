e863f0b71b6a75a2f18b18f52929bae3dc539acd
sireeshareddy <sibbalasireeshareddy31@gmail.com>
2026-02-03 14:16:35 +0530

    df

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 const App=()=>{
   return(
-    <div className="text-3xl font-bold text-center mt-10">Hello guys</div>
+    <div className="text-3xl font-bold text-center mt-10">Hello  ugyfdj guys</div>
   )
 }
 export default App;
